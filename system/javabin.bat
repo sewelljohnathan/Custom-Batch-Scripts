12eb39dc7e5f804fd6a0827c542d9559352d1146
Johnathan <sewelljohnathan@outlook.com>
2022-02-23 15:35:49 -0500

    Added a check for java file not existing.

--- system/javabin.bat
+++ system/javabin.bat
@@ -1,6 +1,10 @@
 :: A script that compiles java files to a bin/ folder, then runs them
 
 @echo off
+if not exist %1.java (
+    echo Could not find %1.java
+    exit /b
+)
 if not exist ../bin/%1.class (
     javac -d ../bin %1.java
 )
