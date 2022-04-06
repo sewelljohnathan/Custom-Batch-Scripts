231b115ce7893162ba3b096d37c7f901e32452a2
Johnathan <sewelljohnathan@outlook.com>
2022-04-05 21:25:38 -0400

    Made some adjustments over time. Most notable is cli flag for showing the diff.

--- src/javatester.sh
+++ src/javatester.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-
 # Compiles and tests a java file against all output files in the directory
 
-# Compile the java file 
-java_file="$1.java";
-if [[ -f $java_file ]]; then
-    javac $java_file
-else
-    echo "Could not find file $java_file"
-    exit 1
-fi
+# Get cli flags
+SHOW_DIFF=false
+for arg in "$@"; do
+    case $arg in
+
+    -s | --show-diff)
+        SHOW_DIFF=true
+        shift
+        ;;
+
+    *)
+        # Compile the java file
+        java_file="$1.java";
+        if [[ -f $java_file ]]; then
+            javac $java_file
+        else
+            echo "Could not find file $java_file"
+            exit 1
+        fi
+
+        ;;
+    esac
+done
+
 
 # Loop through all the files
 for file in *; do
 
-    # Check if the file is a .in
+    # Check if the file is a .in file
     if [ ${file: -3} == .in ]; then
         output_file=${file::-3}.out
-
-    # Check if the file is .in.txt
-    elif [[ ${file: -7} == .in.txt ]]; then
-        output_file=${file::-7}.out.txt
-
-    # Not an input file
     else
         continue
     fi
@@ -31,14 +40,29 @@ for file in *; do
     if [[ -f $output_file ]]; then
 
         # Run the java program
+        echo -n "${file::-3}: "
         java $1 < $file > output.txt
 
         # Find the difference
-        diff -w -B $output_file output.txt
-        correct=$?
+        if [[ $SHOW_DIFF == true ]]; then  
+            diff -w -B $output_file output.txt
+            correct=$?
 
-        if [[ $correct != 0 ]]; then
-            echo "$file Failed"
+            # Only print the success, since errors will be seen
+            if [[ $correct == 0 ]]; then
+                echo "───==≡≡ΣΣ((( つºل͜º)つ"
+            fi
+
+        else
+            diff -w -B $output_file output.txt &> /dev/null
+            correct=$?
+
+            # Print the boolean correctness
+            if [[ $correct == 0 ]]; then
+                echo "───==≡≡ΣΣ((( つºل͜º)つ"
+            else
+                echo ":'("
+            fi
         fi
 
     # Could not find the output file
